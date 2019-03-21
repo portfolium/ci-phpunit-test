55fb20edb5d2968361091a361bfa123b42f8f67b
jeisberg <john@portfolium.com>
2019-03-21 14:54:03 -0700

    chore(PHPUnit): pipe to php-parser 4.0 (#1)
    feat(TestingAutomation): Override CI defaults
    
    Library makes assumptions about the installation
    of CI which Portfolium did not follow. This
    commit allows portfolium projects to use this
    library
    
    chore(PHPUnit): add the codedeploy check to determine environment
    
    chore(PHPUnit): add the qa environment to the switch
    
    chore(PHPUnit): ignore the sql classes
    
    chore(PHPUnit): ignore the sql classes
    
    code cleanup
    
    chore(gitignore): add in files to ignore after the copy
    
    chore(copy): more cleanup
    
    testing the env variable
    
    testing the env variable
    
    testing the env variable

--- application/tests/Bootstrap.php
+++ application/tests/Bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -53,10 +54,26 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
+
 // This `if` statemant is needed for @runInSeparateProcess
 if (! defined('ENVIRONMENT'))
 {
-	define('ENVIRONMENT', 'testing');
+    /**
+     * We need to determine if this is a CodeDeploy build and if so - we need to
+     * configure the application to connect to the QA database - this can be configured
+     * any way we require but we will need database connectivity of some sort when we
+     * move onto Integration testing and regression. CodeDeploy will place several server
+     * variables into the environment - so check for the existance fof the repo url.
+     */
+
+    // sanity check the server variables
+    if (!empty($_SERVER['CODEBUILD_SOURCE_REPO_URL'])) {
+        // set to local
+        define('ENVIRONMENT', 'qa');
+    } else {
+        // set to local
+        define('ENVIRONMENT', 'local');
+    }
 }
 
 /*
@@ -67,31 +84,9 @@ if (! defined('ENVIRONMENT'))
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-switch (ENVIRONMENT)
-{
-	case 'testing':
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-	break;
 
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-	break;
-
-	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
-}
+error_reporting(-1);
+ini_set('display_errors', 1);
 
 /*
  *---------------------------------------------------------------
@@ -101,7 +96,8 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = '../../system';
+
+$system_path = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'system';
 
 /*
  *---------------------------------------------------------------
@@ -118,7 +114,8 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = '../../application';
+
+$application_folder = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'application';
 
 /*
  *---------------------------------------------------------------
@@ -133,8 +130,8 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$view_folder = '';
 
+$view_folder = '';
 
 /*
  * --------------------------------------------------------------------
@@ -155,15 +152,15 @@ switch (ENVIRONMENT)
  *
  * Un-comment the $routing array below to use this feature
  */
-	// The directory name, relative to the "controllers" directory.  Leave blank
-	// if your controller is not in a sub-directory within the "controllers" one
-	// $routing['directory'] = '';
+// The directory name, relative to the "controllers" directory.  Leave blank
+// if your controller is not in a sub-directory within the "controllers" one
+// $routing['directory'] = '';
 
-	// The controller class file name.  Example:  mycontroller
-	// $routing['controller'] = '';
+// The controller class file name.  Example:  mycontroller
+// $routing['controller'] = '';
 
-	// The controller function you wish to be called.
-	// $routing['function']	= '';
+// The controller function you wish to be called.
+// $routing['function']	= '';
 
 
 /*
@@ -180,8 +177,17 @@ switch (ENVIRONMENT)
  *
  * Un-comment the $assign_to_config array below to use this feature
  */
-	// $assign_to_config['name_of_config_item'] = 'value of config item';
+// $assign_to_config['name_of_config_item'] = 'value of config item';
 
+/*
+ * -------------------------------------------------------------------
+ *  COMPOSER VALUES
+ * -------------------------------------------------------------------
+ *
+ * Created by jbackert to get the unit tests to run
+ */
+
+$composer_folder = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR;
 
 
 // --------------------------------------------------------------------
@@ -194,130 +200,163 @@ switch (ENVIRONMENT)
  * ---------------------------------------------------------------
  */
 
-	// Set the current directory correctly for CLI requests
+// Set the current directory correctly for CLI requests
 //	if (defined('STDIN'))
 //	{
-		// This is needed for @runInSeparateProcess
-		chdir(dirname(__FILE__));
+// This is needed for @runInSeparateProcess
+chdir(dirname(__FILE__));
 //	}
 
-	if (($_temp = realpath($system_path)) !== FALSE)
-	{
-		$system_path = $_temp.DIRECTORY_SEPARATOR;
-	}
-	else
-	{
-		// Ensure there's a trailing slash
-		$system_path = strtr(
-			rtrim($system_path, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		).DIRECTORY_SEPARATOR;
-	}
+if (($_temp = realpath($system_path)) !== FALSE)
+{
+    $system_path = $_temp.DIRECTORY_SEPARATOR;
+}
+else
+{
+    // Ensure there's a trailing slash
+    $system_path = strtr(
+            rtrim($system_path, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        ).DIRECTORY_SEPARATOR;
+}
 
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-		exit(3); // EXIT_CONFIG
-	}
+// Is the system path correct?
+if ( ! is_dir($system_path))
+{
+    header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+    echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+    exit(3); // EXIT_CONFIG
+}
 
 /*
  * -------------------------------------------------------------------
  *  Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+// The name of THIS file
+define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-	// Path to the test directory containing all the test files.
-	define('TESTPATH', __dir__.DIRECTORY_SEPARATOR);  // Should be the folder this `Bootstrap.php` file is in.
+// Path to the test directory containing all the test files.
+define('TESTPATH', __dir__.DIRECTORY_SEPARATOR);  // Should be the folder this `Bootstrap.php` file is in.
 
-	// Path to the system directory
-	define('BASEPATH', $system_path);
+// Path to the system directory
+define('BASEPATH', $system_path);
 
-	// Path to the front controller (this file) directory
-	define('FCPATH', realpath(dirname(__FILE__).'/../..').DIRECTORY_SEPARATOR);
+// Path to the front controller (this file) directory
+define('FCPATH', realpath(dirname(__FILE__).'/../../public').DIRECTORY_SEPARATOR);
 
-	// Name of the "system" directory
-	define('SYSDIR', basename(BASEPATH));
+// Name of the "system" directory
+define('SYSDIR', basename(BASEPATH));
 
-	// The path to the "application" directory
-	if (is_dir($application_folder))
-	{
-		if (($_temp = realpath($application_folder)) !== FALSE)
-		{
-			$application_folder = $_temp;
-		}
-		else
-		{
-			$application_folder = strtr(
-				rtrim($application_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
-		}
-	}
-	elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
-	{
-		$application_folder = BASEPATH.strtr(
-			trim($application_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
-	}
-	else
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
-	}
+// required slug for the cname application
+define('PORTFOLIUM_CNAME', 'cname');
 
-	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+// The path to the "application" directory
+if (is_dir($application_folder))
+{
+    if (($_temp = realpath($application_folder)) !== FALSE)
+    {
+        $application_folder = $_temp;
+    }
+    else
+    {
+        $application_folder = strtr(
+            rtrim($application_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        );
+    }
+}
+elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
+{
+    $application_folder = BASEPATH.strtr(
+            trim($application_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        );
+}
+else
+{
+    header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+    echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+    exit(3); // EXIT_CONFIG
+}
 
-	// The path to the "views" directory
-	if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.'views';
-	}
-	elseif (is_dir($view_folder))
-	{
-		if (($_temp = realpath($view_folder)) !== FALSE)
-		{
-			$view_folder = $_temp;
-		}
-		else
-		{
-			$view_folder = strtr(
-				rtrim($view_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
-		}
-	}
-	elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.strtr(
-			trim($view_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
-	}
-	else
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
-	}
+define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
-	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
+// The path to the "views" directory
+if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
+{
+    $view_folder = APPPATH.'views';
+}
+elseif (is_dir($view_folder))
+{
+    if (($_temp = realpath($view_folder)) !== FALSE)
+    {
+        $view_folder = $_temp;
+    }
+    else
+    {
+        $view_folder = strtr(
+            rtrim($view_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        );
+    }
+}
+elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
+{
+    $view_folder = APPPATH.strtr(
+            trim($view_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        );
+}
+else
+{
+    header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+    echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+    exit(3); // EXIT_CONFIG
+}
+
+define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
+
+/*
+ * -------------------------------------------------------------------
+ *  Adding COMPOSERPATH constant
+ * -------------------------------------------------------------------
+ */
+// The path to the "composer" folder
+if (is_dir($composer_folder)) {
+
+    if (($_temp = realpath($composer_folder)) !== FALSE)
+    {
+        $composer_folder = $_temp;
+    }
+    else
+    {
+        $composer_folder = strtr(
+            rtrim($composer_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        );
+    }
+
+    define('COMPOSERPATH', $composer_folder . '/');
+} else {
+    if (!is_dir(BASEPATH . $composer_folder . '/')) {
+        exit("Your composer folder path does not appear to be set correctly. Please open the following file and correct this: " . SELF);
+    }
+
+    define('COMPOSERPATH', BASEPATH . $composer_folder . '/');
+}
 
 /*
  * -------------------------------------------------------------------
  *  Enabling Monkey Patching
  * -------------------------------------------------------------------
- * 
+ *
  * If you want to use monkey patching, uncomment below code and configure
  * for your application.
  */
--- application/tests/_ci_phpunit_test/CIPHPUnitTest.php
+++ application/tests/_ci_phpunit_test/CIPHPUnitTest.php
@@ -10,177 +10,191 @@
 
 class CIPHPUnitTest
 {
-	private static $loader_class = 'CI_Loader';
-	private static $autoload_dirs;
+    private static $loader_class = 'CI_Loader';
+    private static $autoload_dirs;
 
-	/**
-	 * Initialize CIPHPUnitTest
-	 *
-	 * @param array $autoload_dirs directories to search class file for autoloader
-	 */
-	public static function init(array $autoload_dirs = null)
-	{
-		if (! defined('TESTPATH')) {
-			define('TESTPATH', APPPATH.'tests'.DIRECTORY_SEPARATOR);
-		}
+    /**
+     * Initialize CIPHPUnitTest
+     *
+     * @param array $autoload_dirs directories to search class file for autoloader
+     */
+    public static function init(array $autoload_dirs = null)
+    {
+        if (! defined('TESTPATH')) {
+            define('TESTPATH', APPPATH.'tests'.DIRECTORY_SEPARATOR);
+        }
 
-		// Fix CLI args
-		$_server_backup = $_SERVER;
-		$_SERVER['argv'] = [
-			'index.php',
-			'welcome'	// Dummy
-		];
-		$_SERVER['argc'] = 2;
+        // Fix CLI args
+        $_server_backup = $_SERVER;
+        $_SERVER['argv'] = [
+            'index.php',
+            'welcome'	// Dummy
+        ];
+        $_SERVER['argc'] = 2;
 
-		self::$autoload_dirs = $autoload_dirs;
+        self::$autoload_dirs = $autoload_dirs;
 
-		$cwd_backup = getcwd();
+        $cwd_backup = getcwd();
 
-		// Load autoloader for ci-phpunit-test
-		require __DIR__ . '/autoloader.php';
+        // Load autoloader for ci-phpunit-test
+        require __DIR__ . '/autoloader.php';
 
-		require TESTPATH . 'TestCase.php';
+        require TESTPATH . 'TestCase.php';
 
-		$db_test_case_file = TESTPATH . 'DbTestCase.php';
-		if (is_readable($db_test_case_file))
-		{
-			require $db_test_case_file;
-		}
+        $db_test_case_file = TESTPATH . 'DbTestCase.php';
+        if (is_readable($db_test_case_file))
+        {
+            require $db_test_case_file;
+        }
 
-		$unit_test_case_file = TESTPATH . 'UnitTestCase.php';
-		if (is_readable($unit_test_case_file))
-		{
-			require $unit_test_case_file;
-		}
+        $unit_test_case_file = TESTPATH . 'UnitTestCase.php';
+        if (is_readable($unit_test_case_file))
+        {
+            require $unit_test_case_file;
+        }
 
-		// Replace a few Common functions
-		require __DIR__ . '/replacing/core/Common.php';
-		require BASEPATH . 'core/Common.php';
+        /*
+         * --------------------------------------------------------------------
+         * LOAD THE Common overrides for CI
+         * --------------------------------------------------------------------
+         *
+         * This was necessary b/c we override a bunch of common functionality
+         * Also NOTE: I wrapped set_header_function in /replacing/core/Common.php with a check if the function is defined
+         */
+        require_once APPPATH . 'core/MY_Common.php';
 
-		// Workaround for missing CodeIgniter's error handler
-		// See https://github.com/kenjis/ci-phpunit-test/issues/37
-		set_error_handler('_error_handler');
 
-		// Load new functions of CIPHPUnitTest
-		require __DIR__ . '/functions.php';
-		// Load ci-phpunit-test CI_Loader
-		require __DIR__ . '/replacing/core/Loader.php';
-		// Load ci-phpunit-test CI_Input
-		require __DIR__ . '/replacing/core/Input.php';
-		// Load ci-phpunit-test CI_Output
-		require __DIR__ . '/replacing/core/Output.php';
 
-		// Change current directory
-		chdir(FCPATH);
+        // Replace a few Common functions
+        require __DIR__ . '/replacing/core/Common.php';
+        require BASEPATH . 'core/Common.php';
 
-		/*
-		 * --------------------------------------------------------------------
-		 * LOAD THE BOOTSTRAP FILE
-		 * --------------------------------------------------------------------
-		 *
-		 * And away we go...
-		 */
-		require __DIR__ . '/replacing/core/CodeIgniter.php';
+        // Workaround for missing CodeIgniter's error handler
+        // See https://github.com/kenjis/ci-phpunit-test/issues/37
+        set_error_handler('_error_handler');
 
-		self::replaceHelpers();
+        // Load new functions of CIPHPUnitTest
+        require __DIR__ . '/functions.php';
+        // Load ci-phpunit-test CI_Loader
+        require __DIR__ . '/replacing/core/Loader.php';
+        // Load ci-phpunit-test CI_Input
+        require __DIR__ . '/replacing/core/Input.php';
+        // Load ci-phpunit-test CI_Output
+        require __DIR__ . '/replacing/core/Output.php';
 
-		// Create CodeIgniter instance
-		if (! self::wiredesignzHmvcInstalled())
-		{
-			new CI_Controller();
-		}
-		else
-		{
-			new MX_Controller();
-		}
+        // Change current directory
+        chdir(FCPATH);
 
-		// This code is here, not to cause errors with HMVC
-		self::replaceLoader();
 
-		// Restore $_SERVER. We need this for NetBeans
-		$_SERVER = $_server_backup;
 
-		// Restore cwd to use `Usage: phpunit [options] <directory>`
-		chdir($cwd_backup);
-	}
+        /*
+         * --------------------------------------------------------------------
+         * LOAD THE BOOTSTRAP FILE
+         * --------------------------------------------------------------------
+         *
+         * And away we go...
+         */
+        require __DIR__ . '/replacing/core/CodeIgniter.php';
 
-	public static function createCodeIgniterInstance()
-	{
-		if (! self::wiredesignzHmvcInstalled())
-		{
-			new CI_Controller();
-		}
-		else
-		{
-			new CI();
-			new MX_Controller();
-		}
-	}
+        self::replaceHelpers();
 
-	public static function wiredesignzHmvcInstalled()
-	{
-		if (file_exists(APPPATH.'third_party/MX'))
-		{
-			return true;
-		}
+        // Create CodeIgniter instance
+        if (! self::wiredesignzHmvcInstalled())
+        {
+            new CI_Controller();
+        }
+        else
+        {
+            new MX_Controller();
+        }
 
-		return false;
-	}
+        // This code is here, not to cause errors with HMVC
+        self::replaceLoader();
 
-	public static function getAutoloadDirs()
-	{
-		return self::$autoload_dirs;
-	}
+        // Restore $_SERVER. We need this for NetBeans
+        $_SERVER = $_server_backup;
 
-	protected static function replaceLoader()
-	{
-		$my_loader_file =
-			APPPATH . 'core/' . config_item('subclass_prefix') . 'Loader.php';
+        // Restore cwd to use `Usage: phpunit [options] <directory>`
+        chdir($cwd_backup);
+    }
 
-		if (file_exists($my_loader_file))
-		{
-			self::$loader_class = config_item('subclass_prefix') . 'Loader';
-			if ( ! class_exists(self::$loader_class))
-			{
-				require $my_loader_file;
-			}
-		}
-		self::loadLoader();
-	}
+    public static function createCodeIgniterInstance()
+    {
+        if (! self::wiredesignzHmvcInstalled())
+        {
+            new CI_Controller();
+        }
+        else
+        {
+            new CI();
+            new MX_Controller();
+        }
+    }
 
-	protected static function replaceHelpers()
-	{
-		$helpers = ['url_helper', 'download_helper'];
-		foreach ($helpers as $helper) {
-			static::loadHelper($helper);
-		}
-	}
+    public static function wiredesignzHmvcInstalled()
+    {
+        if (file_exists(APPPATH.'third_party/MX'))
+        {
+            return true;
+        }
 
-	protected static function loadHelper($helper)
-	{
-		$my_helper_file = APPPATH . 'helpers/' . config_item('subclass_prefix') . $helper . '.php';
-		if (file_exists($my_helper_file))
-		{
-			require $my_helper_file;
-		}
-		require __DIR__ . '/replacing/helpers/' . $helper . '.php';
-	}
+        return false;
+    }
 
-	public static function setPatcherCacheDir($dir = null)
-	{
-		if ($dir === null)
-		{
-			$dir = TESTPATH . '_ci_phpunit_test/tmp/cache';
-		}
+    public static function getAutoloadDirs()
+    {
+        return self::$autoload_dirs;
+    }
 
-		MonkeyPatchManager::setCacheDir(
-			$dir
-		);
-	}
+    protected static function replaceLoader()
+    {
+        $my_loader_file =
+            APPPATH . 'core/' . config_item('subclass_prefix') . 'Loader.php';
 
-	public static function loadLoader()
-	{
-		$loader = new self::$loader_class;
-		load_class_instance('Loader', $loader);
-	}
+        if (file_exists($my_loader_file))
+        {
+            self::$loader_class = config_item('subclass_prefix') . 'Loader';
+            if ( ! class_exists(self::$loader_class))
+            {
+                require $my_loader_file;
+            }
+        }
+        self::loadLoader();
+    }
+
+    protected static function replaceHelpers()
+    {
+        $helpers = ['url_helper', 'download_helper'];
+        foreach ($helpers as $helper) {
+            static::loadHelper($helper);
+        }
+    }
+
+    protected static function loadHelper($helper)
+    {
+        $my_helper_file = APPPATH . 'helpers/' . config_item('subclass_prefix') . $helper . '.php';
+        if (file_exists($my_helper_file))
+        {
+            require $my_helper_file;
+        }
+        require __DIR__ . '/replacing/helpers/' . $helper . '.php';
+    }
+
+    public static function setPatcherCacheDir($dir = null)
+    {
+        if ($dir === null)
+        {
+            $dir = TESTPATH . '_ci_phpunit_test/tmp/cache';
+        }
+
+        MonkeyPatchManager::setCacheDir(
+            $dir
+        );
+    }
+
+    public static function loadLoader()
+    {
+        $loader = new self::$loader_class;
+        load_class_instance('Loader', $loader);
+    }
 }
